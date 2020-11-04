688a564e4e571d8961b4972b82ea0a872526ef2f
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-04 05:08:12 +0200

    5.1. Mount redux store to project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import questions from './mocks/questions.js';
+import {reducer} from './store/reducer.js';
 
 
 const Settings = {
   MISTAKES_COUNT: 4,
 };
 
+const store = createStore(reducer);
+
 
 ReactDOM.render(
-    <App
-      mistakesCount={Settings.MISTAKES_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        mistakesCount={Settings.MISTAKES_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.getElementById(`root`)
 );
