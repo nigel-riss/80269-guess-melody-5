1281e9b572e841071751a5a0457b7055eba93a8b
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-11-20 06:04:51 +0200

    7.2. Refactor Login component

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,30 +1,93 @@
-import React from 'react';
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from '../../store/api-actions.js';
 
 
-const Login = () => {
-  return (
-    <section className="login">
-      <div className="login__logo">
-        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
-      </div>
-      <h2 className="login__title">Вы настоящий меломан!</h2>
-      <p className="login__text">Хотите узнать свой результат? Представтесь!</p>
-      <form className="login__form" action="">
-        <p className="login__field">
-          <label className="login__label" htmlFor="name">Логин</label>
-          <input className="login__input" type="text" name="name" id="name"/>
-        </p>
-        <p className="login__field">
-          <label className="login__label" htmlFor="password">Пароль</label>
-          <input className="login__input" type="text" name="password" id="password"/>
-          <span className="login__error">Неверный пароль</span>
-        </p>
-        <button className="login__button button" type="submit">Войти</button>
-      </form>
-      <button className="replay" type="button">Сыграть ещё раз</button>
-    </section>
-  );
+class Login extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.loginRef = createRef();
+    this.passwordRef = createRef();
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(e) {
+    const {onSubmit} = this.props;
+
+    e.preventDefault();
+
+    onSubmit({
+      login: this.loginRef.current.value,
+      password: this.passwordRef.current.value,
+    });
+  }
+
+  render() {
+    const {onReplayButtonClick} = this.props;
+
+    return (
+      <section className="login">
+        <div className="login__logo">
+          <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+        </div>
+        <h2 className="login__title">Вы настоящий меломан!</h2>
+        <p className="login__text">Хотите узнать свой результат? Представтесь!</p>
+        <form
+          action=""
+          className="login__form"
+          onSubmit={this._handleSubmit}
+        >
+          <p className="login__field">
+            <label className="login__label" htmlFor="name">Логин</label>
+            <input
+              className="login__input"
+              id="name"
+              name="name"
+              ref={this.loginRef}
+              type="text"
+            />
+          </p>
+          <p className="login__field">
+            <label className="login__label" htmlFor="password">Пароль</label>
+            <input
+              className="login__input"
+              id="password"
+              name="password"
+              ref={this.passwordRef}
+              type="text"
+            />
+            <span className="login__error">Неверный пароль</span>
+          </p>
+          <button className="login__button button" type="submit">Войти</button>
+        </form>
+        <button
+          className="replay"
+          onClick={onReplayButtonClick}
+          type="button"
+        >
+          Сыграть ещё раз
+        </button>
+      </section>
+    );
+  }
+}
+
+
+Login.propTypes = {
+  onReplayButtonClick: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(authData) {
+    dispatch(login(authData));
+  },
+});
+
+
+export {Login};
+export default connect(null, mapDispatchToProps)(Login);
